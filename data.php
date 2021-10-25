35f8b6586dfafacae78b689b106fd74e5194b664
Anton Romanov <609990@gmail.com>
2021-10-25 14:46:51 +0300

    Добавляет функцию времени

--- data.php
+++ data.php
@@ -14,42 +14,48 @@ $lots = [
         'title' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 10999,
-        'path' => 'img/lot-1.jpg'
+        'path' => 'img/lot-1.jpg',
+        'expiration' => '2021-10-25 15:20'
     ],
 
     [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 159999,
-        'path' => 'img/lot-2.jpg'
+        'path' => 'img/lot-2.jpg',
+        'expiration' => '2021-10-26'
     ],
 
     [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' => 	8000,
-        'path' => 'img/lot-3.jpg'
+        'path' => 'img/lot-3.jpg',
+        'expiration' => '2021-10-28'
     ],
 
     [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => 10999,
-        'path' => 'img/lot-4.jpg'
+        'path' => 'img/lot-4.jpg',
+        'expiration' => '2021-10-29'
     ],
 
     [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => 7500,
-        'path' => 'img/lot-5.jpg'
+        'path' => 'img/lot-5.jpg',
+        'expiration' => '2021-10-31'
     ],
 
     [
         'title' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
-        'path' => 'img/lot-6.jpg'
+        'path' => 'img/lot-6.jpg',
+        'expiration' => '2021-10-30'
     ]
 
 ];
--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ require_once("helpers.php");
  * @return @output Сумма в виде числа с добавленным к нему знаком рубля
  */
 
-function decoratePrice ($input) {
+function is_format_price ($input) {
     $output = "";
     $input = ceil($input);
 
@@ -23,6 +23,60 @@ function decoratePrice ($input) {
     $output = $input . " " . "&#8381;";
 
     return $output;
+}
+
+/**
+ * 'Форматирует время оставшееся до окончания лота'
+ *
+ * @param $input Дата в виде строки
+ *
+ * @return @output Время в виде массива
+ */
+
+function is_format_date ($input) {
+
+    $hour = 3600;
+    $minute = 60;
+
+    $date01 = strtotime($input);
+    $date02 = strtotime('now');
+
+    $diff = $date01 - $date02;
+
+    $hours = floor($diff / $hour);
+
+    $expiration = $diff - ($hours * $hour);
+    $minutes = ceil($expiration / $minute);
+
+    if ($minutes > $minute || $minutes < $minute )  {
+
+        $hours = str_pad($hours, 2, '0', STR_PAD_LEFT);
+        $minutes = str_pad($minutes, 2, '0', STR_PAD_LEFT);
+
+    }
+
+    else {
+
+        $hours = str_pad(1, 2, '0', STR_PAD_LEFT);
+        $minutes = str_pad(0, 2, '0', STR_PAD_LEFT);
+
+    }
+    
+    if ($date02 >= $date01) {
+
+        $hours =  str_pad(0, 2, '0', STR_PAD_LEFT);
+        $minutes =  str_pad(0, 2, '0', STR_PAD_LEFT);
+        
+    }
+
+    $output = [
+
+        'hours' => $hours,
+        'minutes' => $minutes
+
+    ];
+
+    return $output['hours'] . ':' . $output['minutes'];
 }
 
 ?>
