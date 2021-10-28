c8988fbc77384f0521476f03b311448a7e7078b8
Anton Romanov <609990@gmail.com>
2021-10-28 13:36:30 +0300

    Создает схему БД

---
+++ schema.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE yeticave
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_general_ci;
+
+USE yeticave;
+
+CREATE TABLE users (
+id INT AUTO_INCREMENT PRIMARY KEY,
+date DATETIME DEFAULT CURRENT_TIMESTAMP,
+email VARCHAR(256) NOT NULL UNIQUE,
+name VARCHAR(128) NOT NULL,
+password VARCHAR(128) NOT NULL,
+message TEXT
+);
+
+CREATE TABLE categories (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(128) NOT NULL UNIQUE,
+code VARCHAR(128) NOT NULL UNIQUE
+);
+
+CREATE TABLE lots (
+id INT AUTO_INCREMENT PRIMARY KEY,
+datе DATETIME DEFAULT CURRENT_TIMESTAMP,
+title TEXT NOT NULL,
+description TEXT NOT NULL,
+path TEXT,
+price INT NOT NULL,
+expiration DATETIME,
+step INT NOT NULL,
+user_id INT,
+winner_id INT,
+category_id INT,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (winner_id) REFERENCES users(id),
+FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE bets (
+id INT AUTO_INCREMENT PRIMARY KEY,
+date DATETIME DEFAULT CURRENT_TIMESTAMP,
+bet INT NOT NULL,
+user_id INT,
+lot_id INT,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (lot_id) REFERENCES lots(id)
+);
+
+CREATE FULLTEXT INDEX yeticave_search ON lots(title, description);
