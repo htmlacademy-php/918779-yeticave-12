32527d53b499ed1518abb7cae2d60c7ffdd0d021
Anton Romanov <609990@gmail.com>
2022-06-05 01:41:10 +0300

    Исправляет опечатку

--- templates/layout.php
+++ templates/layout.php
@@ -29,7 +29,7 @@
         <nav class="user-menu">
             <?php if ($is_auth) : ?>
             <div class="user-menu__logged">
-                <p><?=htmlspecialchairs($user_name); ?></p>
+                <p><?=htmlspecialchars($user_name); ?></p>
                 <a class="user-menu__bets" href="my-bets.php">Мои ставки</a>
                 <a class="user-menu__logout" href="logout.php">Выход</a>
             </div>
